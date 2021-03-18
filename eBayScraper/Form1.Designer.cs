e048de75d63b595a549d215428e824fb49de4f2b
Jamie Foster <jamiefoster15@hotmail.co.uk>
2021-03-18 14:53:42 +0000

    First commit

---
+++ eBayScraper/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿
+namespace eBayScraper
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.searchBx = new System.Windows.Forms.TextBox();
+            this.resultsBx = new System.Windows.Forms.TextBox();
+            this.vScrollBar1 = new System.Windows.Forms.VScrollBar();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.goBtn = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // searchBx
+            // 
+            this.searchBx.Location = new System.Drawing.Point(152, 12);
+            this.searchBx.Name = "searchBx";
+            this.searchBx.Size = new System.Drawing.Size(489, 23);
+            this.searchBx.TabIndex = 0;
+            // 
+            // resultsBx
+            // 
+            this.resultsBx.BackColor = System.Drawing.Color.White;
+            this.resultsBx.Location = new System.Drawing.Point(84, 155);
+            this.resultsBx.Multiline = true;
+            this.resultsBx.Name = "resultsBx";
+            this.resultsBx.ReadOnly = true;
+            this.resultsBx.Size = new System.Drawing.Size(630, 168);
+            this.resultsBx.TabIndex = 1;
+            // 
+            // vScrollBar1
+            // 
+            this.vScrollBar1.Location = new System.Drawing.Point(717, 155);
+            this.vScrollBar1.Name = "vScrollBar1";
+            this.vScrollBar1.Size = new System.Drawing.Size(17, 168);
+            this.vScrollBar1.TabIndex = 2;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(717, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(80, 76);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(84, 123);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 33);
+            this.textBox3.TabIndex = 4;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(190, 123);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 33);
+            this.textBox4.TabIndex = 5;
+            // 
+            // goBtn
+            // 
+            this.goBtn.Location = new System.Drawing.Point(332, 41);
+            this.goBtn.Name = "goBtn";
+            this.goBtn.Size = new System.Drawing.Size(127, 25);
+            this.goBtn.TabIndex = 6;
+            this.goBtn.Text = "SCAPE";
+            this.goBtn.UseVisualStyleBackColor = true;
+            this.goBtn.Click += new System.EventHandler(this.goBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(813, 343);
+            this.Controls.Add(this.goBtn);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.vScrollBar1);
+            this.Controls.Add(this.resultsBx);
+            this.Controls.Add(this.searchBx);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox searchBx;
+        private System.Windows.Forms.TextBox resultsBx;
+        private System.Windows.Forms.VScrollBar vScrollBar1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button goBtn;
+    }
+}
+
---
+++ eBayScraper/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Http;
+
+namespace eBayScraper
+{
+    public partial class Form1 : Form
+    {
+        public String url = "";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void GetHTTP()
+        {
+            HttpClient httpClient = new HttpClient();
+            var html = httpClient.GetAsync(url);
+            if (!html.IsCompleted)
+            {
+                resultsBx.Text = "SCAPING IN PROGESS...";
+
+                if (html.IsCompleted)
+                {
+                    return;
+                }
+            }
+
+            resultsBx.Text = html.Result.ToString();
+        }
+
+        private void goBtn_Click(object sender, EventArgs e)
+        {
+            url = searchBx.Text;
+
+            if (URLIsEmpty(url))
+            {
+                resultsBx.Text = "ffdssd";
+            }
+
+            else
+            {
+                GetHTTP();
+            }
+        }
+
+        private bool URLIsEmpty(String url)
+        {
+            if (url.Length == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
